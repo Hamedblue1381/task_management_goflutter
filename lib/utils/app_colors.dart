ec39395bbf01330d2ef7e090cbf2bf0955290646
HamedBlue <hamed.balanar@gmail.com>
2023-02-18 16:05:58 +0330

    init Comit

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const mainColor = Color(0xFF202e59);
+  static const secondaryColor = Color(0xFF6c8ee3);
+  static const smallTextColor = Color(0xFF2e3253);
+  static const textholder = Color(0xFFedf0f8);
+  static const textGrey = Color(0xFFafb1b5);
+}
