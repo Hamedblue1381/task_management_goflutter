28f9e6ce7ed5bd2ab7fd8637391419f21a8cd703
Hamed Malek Mohammadi <hamed.balanar@gmail.com>
2023-03-09 19:34:24 +0330

    update: get single task const

--- lib/constants/http_consts.dart
+++ lib/constants/http_consts.dart
@@ -2,4 +2,5 @@ class HttpConstants {
   static const String BASE_URL = "http://10.0.2.2:8082";
   static const String GET_TASKS = "/gettasks";
   static const String POST_TASK = "/create";
+  static const String GET_TASK = "/gettask/";
 }
