8928c5500c8d0cb6c36572d806b43d3b49960737
Hamed Malek Mohammadi <hamed.balanar@gmail.com>
2023-03-09 19:40:10 +0330

    feat: add BorderColor & TextColor & readonly

--- lib/widgets/textfield_view.dart
+++ lib/widgets/textfield_view.dart
@@ -1,34 +1,39 @@
 import 'package:flutter/material.dart';
 
-import '../../constants/app_colors.dart';
-
 class TextFieldWidget extends StatelessWidget {
   final TextEditingController textController;
   final String hintText;
   final double? borderRadius;
   final int? maxLines;
+  final bool readOnly;
+  final Color colorText;
+  final Color colorBorder;
   const TextFieldWidget(
       {super.key,
       required this.textController,
       required this.hintText,
       this.borderRadius = 30,
-      this.maxLines = 1});
+      this.maxLines = 1,
+      this.readOnly = false,
+      this.colorBorder = Colors.white,
+      this.colorText = Colors.white});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      readOnly: readOnly,
       maxLines: maxLines,
       controller: textController,
       decoration: InputDecoration(
           filled: true,
-          fillColor: AppColors.textholder,
+          fillColor: colorText,
           hintText: hintText,
           focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(borderRadius!),
-              borderSide: const BorderSide(color: Colors.white, width: 1)),
+              borderSide: BorderSide(color: colorBorder, width: 1)),
           enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(borderRadius!),
-              borderSide: const BorderSide(color: Colors.white, width: 1))),
+              borderSide: BorderSide(color: colorBorder, width: 1))),
     );
   }
 }
