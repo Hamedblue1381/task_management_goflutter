50eda8f3bbfde51d28bbd0f90b164079820a5696
Hamed Malek Mohammadi <hamed.balanar@gmail.com>
2023-03-09 19:39:34 +0330

    refactor: file name

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -3,6 +3,10 @@ import 'package:task_management_goflutter/constants/http_consts.dart';
 
 class DataService extends GetConnect implements GetxService {
   Future<Response> getData(String uri) async {
+    // if (kDebugMode) {
+    //   print(HttpConstants.BASE_URL + uri);
+    // }
+
     Response response = await get(HttpConstants.BASE_URL + uri, headers: {
       'Content-Type': 'application/json charset=UTF-8',
     });
