abe51d9bf4f0ac41290bcb6502d14d671594c35c
Hamed Malek Mohammadi <hamed.balanar@gmail.com>
2023-03-10 13:52:48 +0330

    docs: change color

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const mainColor = Color(0xFF202e59);
-  static const secondaryColor = Color(0xFF6c8ee3);
+  static const secondaryColor = Color.fromARGB(221, 10, 88, 177);
   static const smallTextColor = Color(0xFF2e3253);
   static const textholder = Color(0xFFedf0f8);
   static const textGrey = Color(0xFFafb1b5);
